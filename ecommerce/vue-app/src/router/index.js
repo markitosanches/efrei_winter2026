1c67bd94edd0df139792fe4218475b7011474758
Marcos <markitosanches@gmail.com>
2026-03-24 16:22:01 -0400

    Class 5

--- ecommerce/vue-app/src/router/index.js
+++ ecommerce/vue-app/src/router/index.js
@@ -1,9 +1,13 @@
 import {createRouter, createWebHistory } from 'vue-router'
 
 import Home from '../views/HomeView.vue'
+import ProductView from '../views/ProductView.vue'
+import AddProductView from '../views/AddProductView.vue'
 
 const routes = [
-    {path: '/', name:'home', component: Home}
+    {path: '/', name:'home', component: Home},
+    {path: '/product/:id', name:'product', component: ProductView},
+    {path: '/add-product', name:'add-product', component: AddProductView}
 ]
 
 const router = createRouter({
