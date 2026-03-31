e646b0517d3bc9cd3b23b93548758e923a8e5948
Marcos <markitosanches@gmail.com>
2026-03-31 15:35:07 -0400

    Class 6

---
+++ ecommerce/server/app/controllers/product.controller.js
@@ -0,0 +1,16 @@
+const db = require('../models')
+const Product = db.products
+
+exports.findAll = (req, res) => {
+
+    Product.findAll()
+    .then(data => {
+        res.send(data)
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: 'Some error occured!'
+        })
+    })
+
+}
